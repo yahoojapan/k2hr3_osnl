69ca7f407bf141c4c2db3619fd3833e61fcc4fa3
Hirotaka Wakabayashi <hiwakaba@lycorp.co.jp>
2024-10-17 14:31:12 +0900

    Fixes errors

--- src/tests/test_endpoint.py
+++ src/tests/test_endpoint.py
@@ -115,7 +115,7 @@ class TestNotificationEndpoint(unittest.TestCase):
             new_conf = K2hr3Conf(conf_file_path)
             endpoint.conf = new_conf
         the_exception = cm.exception
-        self.assertRegex("can't set attribute.*", '{}'.format(the_exception))
+        self.assertRegex('{}'.format(the_exception), "can't set attribute.*")
 
     def test_neutron_payload_to_params(self):
         """Checks if _payload_to_params() works correctly.
