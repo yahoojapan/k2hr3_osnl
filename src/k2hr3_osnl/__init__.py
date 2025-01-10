4ba09d443a4e69b3fdf5d935528e43934deeba72
Hirotaka Wakabayashi <hiwkby@yahoo.com>
2025-01-10 17:48:01 +0900

    import typing_extensions

--- src/k2hr3_osnl/__init__.py
+++ src/k2hr3_osnl/__init__.py
@@ -40,6 +40,7 @@ from logging import StreamHandler
 from pathlib import Path
 import sys
 import time
+import typing_extensions
 from typing import List, Set, Dict, Tuple, Optional, Union  # noqa: pylint: disable=unused-import
 
 import oslo_config  # type: ignore
